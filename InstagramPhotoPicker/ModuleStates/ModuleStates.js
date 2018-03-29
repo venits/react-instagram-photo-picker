63223382316178d1927d1b4861337d4709bc35c4
Venits <tomasz.przybyl.it@gmail.com>
2018-03-29 21:25:17 +0200

    Delete old folder

--- InstagramPhotoPicker/ModuleStates/ModuleStates.js
+++
@@ -1,6 +0,0 @@
-export default {
-  LOGIN_TO_INSTAGRAM: 'login_to_instagram',
-  APP_IS_LOADING: 'app_is_loading',
-  NO_INSTAGRAM_PHOTOS: 'no_instagram_photos',
-  PHOTO_PICKER: 'photo_picker',
-};
